3700a4a4a6280f4776562550baea82b5e4ca5ed1
Ross Patterson <me@rpatterson.net>
2017-12-03 08:45:32 -0800

    Fix Windows build steps

--- build-windows.cmd
+++ build-windows.cmd
@@ -1,6 +1,7 @@
 mkdir build
 cd build
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+REM Must be run in the "Developer Command Prompt for VS..."
+REM provided by your Visutal Studio install
 cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ..
-msbuild synergy-core.sln /p:Platform="x64" /p:Configuration=%CMAKE_BUILD_TYPE% /m
+msbuild synergy-core.sln /p:Platform="x64" /m
 cd ..
